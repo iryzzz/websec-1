4da001e3c65cbe9962655130b7244f839b50f0d8
iryzzz <58042184+iryzzz@users.noreply.github.com>
2024-05-10 20:50:19 +0400

    Исправлены ошибки в скрипте

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var last_result = "";
 
+function isNumber(n) 
+{ 
+    return !isNaN(parseFloat(n)) && !isNaN(n - 0);
+}
+
 function buttonClickHandler()
 {
     let left = document.getElementById("left").value;
@@ -8,22 +13,26 @@ function buttonClickHandler()
     if (left != "" && right != ""){
         var result = "";
 
+        if (!isNumber(left) || !isNumber(right)){
+            alert("Ошибка: операнды не являются числами.")
+            return;
+        }
         switch (document.getElementById("select1").value){
             case 'sum':
                 result = left + "+" + right + "=" + (parseFloat(left) + parseFloat(right));
                 break;
             case 'dif':
-                result = left + "+" + right + "=" + (parseFloat(left) - parseFloat(right));
+                result = left + "-" + right + "=" + (parseFloat(left) - parseFloat(right));
                 break;
             case 'mul':
-                result = left + "+" + right + "=" + (parseFloat(left) * parseFloat(right));
+                result = left + "*" + right + "=" + (parseFloat(left) * parseFloat(right));
                 break;
             case 'div':
                 if (Math.abs(right) < Number.EPSILON){
                     alert("Ошибка: деление на ноль.")
                     return;
                 }
-                result = left + "+" + right + "=" + (parseFloat(left) / parseFloat(right));
+                result = left + "/" + right + "=" + (parseFloat(left) / parseFloat(right));
                 break;
         }     
         document.getElementById("results").innerHTML = '<p style="opacity: 0.5;">' + last_result + "</p>" + "<p>" + result + "</p>";
